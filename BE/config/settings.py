5740771eab91d3e001e8a23e1c4d33400208f0bc
Haya Qonita <qonitahaya@gmail.com>
2026-06-10 14:03:51 +0700

    DELETE: serializer login

--- BE/config/settings.py
+++ BE/config/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ALGORITHM": "HS256",
     "AUTH_HEADER_TYPES": ("Bearer",),
