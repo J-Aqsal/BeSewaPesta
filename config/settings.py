c9aa58a1bb6605784e388f76ec13b3602b52bc19
J-Aqsal <aqsalacc@gmail.com>
2026-07-05 13:42:32 +0700

    FIX: allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.168.101.68', 'localhost', '10.178.162.16', '127.0.0.1']
+ALLOWED_HOSTS = ['172.168.101.68', 'localhost', '10.178.162.16', '127.0.0.1', '143.198.196.223']
 
 
 # Application definition
